e99772b90a8a7b1a5ad1edf7517e39fee77a89f9
Alex Prado <alexanders.prado@interseguro.com.pe>
2025-04-28 19:48:50 -0500

    Plantilla básica

---
+++ src/app/app.config.ts
@@ -0,0 +1,69 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import Material from '@primeng/themes/material';
+import { definePreset } from '@primeng/themes';
+import { MessageService } from 'primeng/api';
+
+import { routes } from './app.routes';
+import { getSingleSpaExtraProviders } from 'single-spa-angular';
+import { APP_BASE_HREF } from '@angular/common';
+
+const MyPreset = definePreset(Material, {
+  semantic: {
+    primary: {
+      50: '{indigo.50}',
+      100: '{indigo.100}',
+      200: '{indigo.200}',
+      300: '{indigo.300}',
+      400: '{indigo.400}',
+      500: '{indigo.500}',
+      600: '{indigo.600}',
+      700: '{indigo.700}',
+      800: '{indigo.800}',
+      900: '{indigo.900}',
+      950: '{indigo.950}'
+    },
+    surface: {
+      50: '#ffffff',
+      950: '#1a1a1a'
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{primary.500}',
+          contrastColor: '#ffffff'
+        }
+      },
+      dark: {
+        primary: {
+          color: '{primary.100}',
+          contrastColor: '#1a1a1a'
+        }
+      }
+    }
+  }
+});
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes,
+      withEnabledBlockingInitialNavigation()),
+    {
+      provide: APP_BASE_HREF,
+      useValue: '/',
+    },
+    MessageService,
+    getSingleSpaExtraProviders(),
+    providePrimeNG({
+      ripple: true,
+      inputStyle: 'filled',
+      theme: { preset: MyPreset, options: { darkModeSelector: '.app-dark' } },
+    }),
+    provideAnimations(),
+  ],
+};
